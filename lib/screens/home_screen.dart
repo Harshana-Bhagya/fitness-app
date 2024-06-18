b412b0f9291c2ae00d48ccafed70292bba8f72c4
Harshana-Bhagya <harshanabhagya@gmail.com>
2024-06-18 23:55:11 +0530

    add a body to scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FAta'),
+        title: const Text('FAta'),
       ),
+      body: Container(),
     );
   }
 }
