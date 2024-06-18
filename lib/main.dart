1550f5c2ea1544a533d6627be5c4299f2d5d062e
Harshana-Bhagya <harshanabhagya@gmail.com>
2024-06-18 23:57:08 +0530

    integrate home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class FitnessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
